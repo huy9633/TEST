1b1ccf983aaf7067ff1298fae9cadb301d4b79c2
Tuanle125 <39647768+Tuanle125@users.noreply.github.com>
2021-10-23 16:23:40 +0700

    Update CheckAndReport.h

--- Library/CheckAndReport.h
+++ Library/CheckAndReport.h
@@ -1,24 +1,24 @@
 #ifndef _CHECKANDREPORT_
 #define _CHECKANHREPORT_
-#include <iostream>
 #include "orders.h"
 #include "goods.h"
-#include <fstream>
 #include "GoodManagement.h"
 
 
-void CheckAndReport(vector<goods>& hanghoa, vector<orders>& dh)
-{
+void CHECKANDREPORT(vector<goods> hanghoa) {
 	Input(hanghoa);
 	orders x;
 	bool test = false;
 	int count = 1, i;
+
 	ofstream os;
-	os.open("DonHang.txt");
-	for (i = 0; i < count;)
-	{
-		cout << "STT " << count << endl;
+	os.open("DonHang.txt", fstream::app);
+
+	for (i = 0; i < count;) {
+		x.number = count;
+		// nhap don hang
 		cin >> x;
+		
 		for (int i = 0; i < hanghoa.size(); i++)
 		{
 			if (hanghoa[i].seri == x.seri)
@@ -27,18 +27,18 @@ void CheckAndReport(vector<goods>& hanghoa, vector<orders>& dh)
 				//  hang hoa trong kho phai lon hon hoac bang so luong hang hoa nhap
 				if (hanghoa[i].amount >= x.amount)
 				{
+					x.calculateTotalMoney(hanghoa); // ham tu tinh tong tien
+					//xuat ra tong tien : x.total_money;
 					hanghoa[i].amount -= x.amount;
-					os << "STT " << count << endl;
 					os << x << endl;
 					count++;
 					cout << "Quy khach da dang ky thanh cong. Don hang dang cho xu ly";
-					dh.push_back(x);
+					//[ESC]
 					Sleep(2000);
 					system("cls");
 					break;
 				}
-				else
-				{
+				else {
 					test = true;
 					cout << "Hien tai khong du so luong hang hoa. Quy khach vui long chon hang hoa khac. Xin cam on";
 					Sleep(2000);
@@ -58,6 +58,6 @@ void CheckAndReport(vector<goods>& hanghoa, vector<orders>& dh)
 		test = false;
 	}
 	os.close();
-	
+
 }
 #endif
